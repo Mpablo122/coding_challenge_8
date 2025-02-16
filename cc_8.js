dfb7d0a99ceed242e5bb82863267ec3c88cb523b
Mark Francis Pablo <markfrancispablojr@gmail.com>
2025-02-16 15:51:07 -0500

    task 2 product price after discount

--- cc_8.js
+++ cc_8.js
@@ -5,6 +5,15 @@ function caculateSalary(baseSalary, bonus, taxrate) {
     console.log(`Net Salary: $${netSalary}`)
 };
 
-caculateSalary(5000,500,0.1);
-caculateSalary(7000,1000,0.15);
+caculateSalary(5000,500,0.1); //final output should be
+caculateSalary(7000,1000,0.15); //final output should be
 
+//task 2 product price after discount
+
+function calculateDiscount(price, discountRate) {
+    let finalPrice = price - (price * discountRate)
+    console.log(`Fianl price: $${finalPrice}`)
+};
+
+calculateDiscount(100,0.2); // final output should be 80.00
+calculateDiscount(250,0.15); // final output should be $212.50
